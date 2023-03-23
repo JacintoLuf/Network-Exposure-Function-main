5632e8691023539c7ebe470d6b299ebb52654f64
Jacinto Luf <jacinto.luf@ua.pt>
2023-03-23 16:28:05 +0000

    dummy nef

---
+++ README.md
@@ -0,0 +1,2 @@
+# Network-Exposure-Function
+NEF implementation and integration with Open5GS 5G Core
---
+++ dummy_nef/main.py
@@ -0,0 +1,24 @@
+import requests
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Hello, world!"
+
+@app.route('/test', methods=["GET"])
+def dummy_call():
+    endpoint = "http://10.244.2.42:80/nnrf-nfm/v1/nf-instances"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+
+    response = requests.get(endpoint, headers=headers)
+    print(response.status_code)
+    print(response.content)
+
+    return response.text
